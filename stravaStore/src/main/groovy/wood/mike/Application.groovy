bcee31146a80eb99638f93797af01d146052aff0
leadtrip <woodm@objectcomputing.com>
2023-02-07 16:03:28 +0000

    Add swagger UI support to StravaStore

--- stravaStore/src/main/groovy/wood/mike/Application.groovy
+++ stravaStore/src/main/groovy/wood/mike/Application.groovy
@@ -2,9 +2,16 @@ package wood.mike
 
 import io.micronaut.context.env.Environment
 import io.micronaut.runtime.Micronaut
-import groovy.transform.CompileStatic
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
 
-@CompileStatic
+@OpenAPIDefinition(
+        info = @Info(
+                title = "StravaStore",
+                description = 'Stores data retrieved from Strava by the StravaFetch service',
+                version = "0.1"
+        )
+)
 class Application {
 
     static void main(String[] args) {
